c11ce9aa1fefd1193ce741b3acaeaafddc1553e8
hypocrisy1996 <906592802@qq.com>
2021-11-13 11:04:36 +0800

    add check_for_done

--- Tqw/Helpers.py
+++ Tqw/Helpers.py
@@ -5,6 +5,8 @@ Created on Sat Nov 13 10:55:58 2021
 @author: TQW
 """
 import pygame
+import numpy as np
+
 def update_by_man(event,mat):
     """
     This function detects the mouse click on the game window. Update the state matrix of the game. 
@@ -75,3 +77,50 @@ def render(screen, mat):
     draw_board(screen)
     draw_stone(screen, mat)
     pygame.display.update()
+def check_for_done(mat):
+    """
+    please write your own code testing if the game is over. Return a boolean variable done. If one of the players wins
+    or the tie happens, return True. Otherwise return False. Print a message about the result of the game.
+    input: 
+        2D matrix representing the state of the game
+    output:
+        True,1:Black win the game
+        True,0:Draw
+        True,-1:White win the game
+        False,0:Not complete
+    """
+    def _five_mat_res(mat):
+        rowsum = np.sum(mat, 0)
+        colsum = np.sum(mat, 1)
+        diag_sum_tl = mat.trace()
+        diag_sum_tr = mat[::-1].trace()
+
+        player_one_wins = any(rowsum == 5)
+        player_one_wins += any(colsum == 5)
+        player_one_wins += (diag_sum_tl == 5)
+        player_one_wins += (diag_sum_tr == 5)
+
+        if player_one_wins:
+            return 1
+
+        player_two_wins = any(rowsum == -5)
+        player_two_wins += any(colsum == -5)
+        player_two_wins += (diag_sum_tl == -5)
+        player_two_wins += (diag_sum_tr == -5)
+
+        if player_two_wins:
+            return -1
+        # if not over - no result
+        return None
+    if np.all(mat != 0):
+        return True,0.
+    size = mat.shape[0]
+    for i in range(size-5+1):
+        for j in range(size-5+1):
+            #print(self.board[i:i+5,j:j+5])
+            res = _five_mat_res(mat[i:i+5,j:j+5])
+            if res == None:
+                continue
+            else:
+                return True, res
+    return False,0
